a5a6d5cc8e7ecc0a98d20efce10ab2e7f3b2e931
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2020-09-12 18:48:21 -0300

    Uploades sample project

---
+++ csharp-di-console-app/GrettingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace di_console_app
+{
+    public interface IGrettingService
+    {
+        void Run();
+    }
+
+    public sealed class GrettingService : IGrettingService
+    {
+        private readonly ILogger<GrettingService> _log;
+        private readonly IConfiguration _config;
+
+        public GrettingService(ILogger<GrettingService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogInformation("Run number {runNumber}", i);
+            }
+        }
+    }
+}
---
+++ csharp-di-console-app/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace di_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start: This is for Serilog
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+            // End
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((configure, services) => 
+                {
+                    services.AddTransient<IGrettingService, GrettingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GrettingService>(host.Services);
+            svc.Run();
+        }
+
+        // Start: This is for Serilog
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
+        // End
+    }
+}
